b880e2b4cff5bf5017e73660c4e8d41bf35bd25b
SylvainDev <sylvain.raveneau1@gmail.com>
2025-02-12 22:24:04 +0100

    ajout du show pour voir une box, closes #5

---
+++ resources/views/boxes/index.blade.php
@@ -0,0 +1,39 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Gestion des boxes') }}
+        </h2>
+    </x-slot>
+
+    <a href="{{ route('boxes.create') }}">Créer une box</a>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID Box</th>
+                    <th>Nom de la Box</th>
+                    <th>Contenu</th>
+                    <th>Créateur</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($boxes as $box)
+                <tr>
+                    <td>{{ $box->id }}</td>
+                    <td>{{ $box->name }}</td>
+                    <td>{{ $box->contenu }}</td>
+                    <td>{{ $box->user->name}}</td>
+                    <td>
+                        <a href="{{ route('boxes.show', $box->id) }}">Voir</a>
+                        <a href="{{ route('boxes.edit', $box->id) }}">Éditer</a>
+                        <form action="{{ route('boxes.destroy', $box->id) }}" method="POST" style="display:inline;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Supprimer</button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+</x-app-layout>
